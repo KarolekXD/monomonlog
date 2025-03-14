be67d40c9003cf6d45db1e59a7cd16cf41b3f84e
Czar0l <119510854+KarolekXD@users.noreply.github.com>
2025-03-14 18:57:56 +0100

    Update getMessage.js

--- api/getMessage.js
+++ api/getMessage.js
@@ -3,7 +3,7 @@ import { get } from '@vercel/edge-config';
 export const config = { runtime: 'edge' };
 
 export default async function handler() {
-    const greeting = await get('greeting');
+    const message = await get('message');
     return new Response(JSON.stringify({ message }), { 
         headers: { 'Content-Type': 'application/json' } 
     });
