562615bae32ac0b0dd98084eb2867c069fe55d16
Czar0l <119510854+KarolekXD@users.noreply.github.com>
2025-03-14 18:57:22 +0000

    debugging :3

--- api/getMessage.js
+++ api/getMessage.js
@@ -10,6 +10,6 @@ export default async function handler(req, res) {
     if (error) {
         return res.status(500).json({ error: error.message });
     }
-
+    console.log(data);
     res.status(200).json({ message: data?.message || "No message found" });
 }
