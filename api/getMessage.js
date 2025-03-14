f960cf61737ecb3fa7b8caecd2bd44fa8910f382
Czar0l <119510854+KarolekXD@users.noreply.github.com>
2025-03-14 19:03:55 +0000

    debugging :3

--- api/getMessage.js
+++ api/getMessage.js
@@ -4,12 +4,10 @@ export default async function handler(req, res) {
     const { data, error } = await supabase
         .from('messages')
         .select('message')
-        .eq('id', 1)
-        .single();
+        .single()
 
     if (error) {
         return res.status(500).json({ error: error.message });
-        console.log(data);
     }
     res.status(200).json({ message: data?.message || "No message found" });
 }
